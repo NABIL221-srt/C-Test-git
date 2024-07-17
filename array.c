8a309fb3fe8dc159c8648cc9efea70ea0e359c93
NABIL221-srt <nabil.rahmouni@outlook.com>
2024-07-17 22:07:22 +0100

    this is a small update

--- array.c
+++ array.c
@@ -3,7 +3,8 @@
 int main(int argc, char const *argv[])
 {
     int tableau[5] = {4, 99, 55, 333, 3221};
-printf("here is the starting point of my array);
+    printf("here is the starting point of my array");
+
     for (int i = 0; i < 5; i++)
     {
         printf("this is my array ");
